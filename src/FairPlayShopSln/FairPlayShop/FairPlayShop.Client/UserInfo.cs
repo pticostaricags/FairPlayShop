93db70cc7bf82882e82bb3a1b4d3113bb22dc762
Eduardo Fonseca <efonsecab@gmail.com>
2023-10-19 05:05:28 -0600

    Adding skeleton for FairPlayShop project

---
+++ src/FairPlayShopSln/FairPlayShop/FairPlayShop.Client/UserInfo.cs
@@ -0,0 +1,8 @@
+namespace FairPlayShop.Client
+{
+    public class UserInfo
+    {
+        public required string UserId { get; set; }
+        public required string Email { get; set; }
+    }
+}
---
+++ src/FairPlayShopSln/FairPlayShop/FairPlayShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FairPlayShop.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
