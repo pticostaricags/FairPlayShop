9b6e940838595c3a5465090604fc31a79b7b3110
Eduardo Fonseca <efonsecab@gmail.com>
2023-10-19 05:57:36 -0600

    Adding base test for SeedData

---
+++ src/FairPlayShopSln/FairPlayShop.AutomatedTests/DataAccess/SeedDataTests.cs
@@ -0,0 +1,63 @@
+﻿using FairPlayShop.AutomatedTests.ServerSideServices;
+using FairPlayShop.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MsSql;
+
+namespace FairPlayShop.AutomatedTests.DataAccess
+{
+    [TestClass]
+    public class SeedDataTests
+    {
+        [ClassInitialize]
+#pragma warning disable IDE0060 // Remove unused parameter
+        public static async Task ClassInitializeAsync(TestContext testContext)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            await ServerSideServicesTestBase._msSqlContainer.StartAsync();
+        }
+
+        [ClassCleanup()]
+        public static async Task ClassCleanupAsync()
+        {
+            if (ServerSideServicesTestBase._msSqlContainer.State == DotNet.Testcontainers.Containers.TestcontainersStates.Running)
+            {
+                await ServerSideServicesTestBase._msSqlContainer.StopAsync();
+            }
+        }
+
+        protected static async Task<FairPlayShopDatabaseContext> GetFairPlayShopDatabaseContextAsync()
+        {
+            DbContextOptionsBuilder<FairPlayShopDatabaseContext> dbContextOptionsBuilder =
+                new();
+            dbContextOptionsBuilder.UseSqlServer(ServerSideServicesTestBase._msSqlContainer.GetConnectionString(),
+                sqlServerOptionsAction =>
+                {
+                    sqlServerOptionsAction.EnableRetryOnFailure(
+                            maxRetryCount: 3,
+                            maxRetryDelay: TimeSpan.FromSeconds(3),
+                            errorNumbersToAdd: null);
+                });
+            FairPlayShopDatabaseContext fairPlayShopDatabaseContext =
+                new(dbContextOptionsBuilder.Options);
+            await fairPlayShopDatabaseContext.Database.EnsureCreatedAsync();
+            await fairPlayShopDatabaseContext.Database.ExecuteSqlRawAsync(Properties.Resources.SeedData);
+            return fairPlayShopDatabaseContext;
+        }
+
+        [TestMethod]
+        public async Task Test_SeedData_ExistsAsync()
+        {
+            var ctx = await GetFairPlayShopDatabaseContextAsync();
+            var productStatuses = await ctx.ProductStatus.ToArrayAsync();
+            Assert.IsNotNull(productStatuses);
+            Assert.AreEqual(2, productStatuses.Length);
+            Assert.AreEqual(nameof(FairPlayShop.Common.Enums.ProductStatus.Draft), productStatuses[0].Name);
+            Assert.AreEqual(nameof(FairPlayShop.Common.Enums.ProductStatus.Active), productStatuses[1].Name);
+        }
+    }
+}
--- src/FairPlayShopSln/FairPlayShop.AutomatedTests/TestResources/SeedData.sql
+++ src/FairPlayShopSln/FairPlayShop.AutomatedTests/TestResources/SeedData.sql
@@ -11,19 +11,19 @@ Post-Deployment Script Template
 */
 IF NOT EXISTS
 (
-   SELECT * FROM Product WHERE ProductId = 1
+   SELECT * FROM ProductStatus WHERE ProductStatusId = 1
 )
 BEGIN
-    SET IDENTITY_INSERT [Product] ON
-    INSERT INTO Product(ProductId, [Name]) VALUES(1,'Draft')
-    SET IDENTITY_INSERT [Product] OFF
+    SET IDENTITY_INSERT [ProductStatus] ON
+    INSERT INTO ProductStatus(ProductStatusId, [Name]) VALUES(1,'Draft')
+    SET IDENTITY_INSERT [ProductStatus] OFF
 END
 IF NOT EXISTS
 (
-   SELECT * FROM Product WHERE ProductId = 2
+   SELECT * FROM ProductStatus WHERE ProductStatusId = 2
 )
 BEGIN
-    SET IDENTITY_INSERT [Product] ON
-    INSERT INTO Product(ProductId, [Name]) VALUES(2,'Active')
-    SET IDENTITY_INSERT [Product] OFF
+    SET IDENTITY_INSERT [ProductStatus] ON
+    INSERT INTO ProductStatus(ProductStatusId, [Name]) VALUES(2,'Active')
+    SET IDENTITY_INSERT [ProductStatus] OFF
 END
