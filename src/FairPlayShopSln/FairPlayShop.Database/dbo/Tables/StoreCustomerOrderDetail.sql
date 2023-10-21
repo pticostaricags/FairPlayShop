b246c49346f0801b0b1e4af6dbf16c65dd69add7
Eduardo Fonseca <efonsecab@gmail.com>
2023-10-21 04:58:22 -0600

    Adding base for customer orders

---
+++ src/FairPlayShopSln/FairPlayShop.Database/dbo/Tables/StoreCustomerOrderDetail.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[StoreCustomerOrderDetail]
+(
+	[StoreCustomerOrderDetail] BIGINT NOT NULL CONSTRAINT PK_StoreCustomerOrderDetail PRIMARY KEY IDENTITY, 
+    [StoreCustomerOrderId] BIGINT NOT NULL, 
+    [ProductId] BIGINT NOT NULL, 
+    [UnityPrice] MONEY NOT NULL, 
+    [Quantity] MONEY NOT NULL, 
+    [LineTotal] MONEY NOT NULL,
+    CONSTRAINT [FK_StoreCustomerOrderDetail_StoreCustomerOrder] FOREIGN KEY ([StoreCustomerOrderId]) REFERENCES [StoreCustomerOrder]([StoreCustomerOrderId]), 
+    CONSTRAINT [FK_StoreCustomerOrderDetail_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([ProductId])
+)
