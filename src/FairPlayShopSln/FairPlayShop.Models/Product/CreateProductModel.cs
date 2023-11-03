549615f787a45f93aec77e5ec40eb2df667662aa
Eduardo Fonseca <efonsecab@gmail.com>
2023-11-02 21:23:33 -0600

    Adding localized messages

--- src/FairPlayShopSln/FairPlayShop.Models/Product/CreateProductModel.cs
+++ src/FairPlayShopSln/FairPlayShop.Models/Product/CreateProductModel.cs
@@ -12,39 +12,56 @@ namespace FairPlayShop.Models.Product
 {
     public class CreateProductModel
     {
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.Name_Required))]
+        [StringLength(50, ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.Name_StringLength))]
         public string? Name { get; set; }
 
-        [Required]
-        [StringLength(500)]
+        [Required(ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.Description_Required))]
+        [StringLength(500, ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.Description_StringLength))]
         public string? Description { get; set; }
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.QuantityInStock_Required))]
         public int? QuantityInStock { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.SellingPrice_Required))]
         public decimal? SellingPrice { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.AcquisitionCost_Required))]
         public decimal? AcquisitionCost { get; set; }
 
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.Sku_Required))]
+        [StringLength(50, ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.Sku_StringLength))]
         public string? Sku { get; set; }
 
-        [StringLength(50)]
+        [StringLength(50, ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.Barcode_StringLength))]
         public string? Barcode { get; set; }
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.ProductStatus_Required))]
         public ProductStatus? ProductStatus { get; set; }
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.PhotoName_Required))]
+        [StringLength(50, ErrorMessageResourceType = typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateProductModelLocalizer.PhotoName_StringLength))]
         public string? PhotoName { get; set; }
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessageResourceType =typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName =nameof(CreateProductModelLocalizer.PhotoFilename_Required))]
+        [StringLength(50,ErrorMessageResourceType =typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName =nameof(CreateProductModelLocalizer.PhotoFilename_StringLength))]
         public string? PhotoFilename { get; set; }
-        [Required]
+        [Required(ErrorMessageResourceType =typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName =nameof(CreateProductModelLocalizer.PhotoBytes_Required))]
         public byte[]? PhotoBytes { get; set; }
-        [Required]
+        [Required(ErrorMessageResourceType =typeof(CreateProductModelLocalizer),
+            ErrorMessageResourceName =nameof(CreateProductModelLocalizer.StoreId_Required))]
         public long? StoreId { get; set; }
     }
 
