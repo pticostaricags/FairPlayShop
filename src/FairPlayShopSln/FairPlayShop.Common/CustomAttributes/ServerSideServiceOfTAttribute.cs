31fc7c2297f09413a728eab506411f6d66d61d32
Eduardo Fonseca <efonsecab@gmail.com>
2023-11-10 17:01:12 -0600

    Adding Sonar Analyzer to Common project

--- src/FairPlayShopSln/FairPlayShop.Common/CustomAttributes/ServerSideServiceOfTAttribute.cs
+++ src/FairPlayShopSln/FairPlayShop.Common/CustomAttributes/ServerSideServiceOfTAttribute.cs
@@ -1,5 +1,8 @@
-﻿namespace FairPlayShop.Common.CustomAttributes
+﻿using System.Runtime.CompilerServices;
+
+namespace FairPlayShop.Common.CustomAttributes
 {
+    [CompilerGenerated]
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class ServerSideServiceOfTAttribute<T> : Attribute where T : class
     {
