ad8dcc1953995758e23355230e8fd6f0110b02eb
Eduardo Fonseca <efonsecab@gmail.com>
2023-11-12 07:43:03 -0600

    Completing localization

--- src/FairPlayShopSln/FairPlayShop.Models/StoreCustomerOrder/CreateStoreCustomerOrderModel.cs
+++ src/FairPlayShopSln/FairPlayShop.Models/StoreCustomerOrder/CreateStoreCustomerOrderModel.cs
@@ -10,8 +10,14 @@ namespace FairPlayShop.Models.StoreCustomerOrder
         [Required(ErrorMessageResourceType = typeof(CreateStoreCustomerOrderModelLocalizer),
             ErrorMessageResourceName = nameof(CreateStoreCustomerOrderModelLocalizer.StoreCustomerId_Required))]
         public long? StoreCustomerId { get; set; }
+        [Display(ResourceType = typeof(CreateStoreCustomerOrderModelLocalizer),
+            Name = nameof(CreateStoreCustomerOrderModelLocalizer.OrderSubTotalText))]
         public decimal OrderSubTotal => CreateStoreCustomerOrderDetailModel!.Sum(p => p.LineTotal.GetValueOrDefault(0));
+        [Display(ResourceType = typeof(CreateStoreCustomerOrderModelLocalizer),
+            Name = nameof(CreateStoreCustomerOrderModelLocalizer.OrderTotalText))]
         public decimal OrderTotal => OrderSubTotal + TaxTotal;
+        [Display(ResourceType = typeof(CreateStoreCustomerOrderModelLocalizer),
+            Name = nameof(CreateStoreCustomerOrderModelLocalizer.TaxTotalText))]
         public decimal TaxTotal => OrderSubTotal * ((decimal)13 / 100);
         [Required(ErrorMessageResourceType = typeof(CreateStoreCustomerOrderModelLocalizer),
             ErrorMessageResourceName = nameof(CreateStoreCustomerOrderModelLocalizer.CreateStoreCustomerOrderDetailModel_Required))]
@@ -26,6 +32,14 @@ namespace FairPlayShop.Models.StoreCustomerOrder
     [LocalizerOfT<CreateStoreCustomerOrderModel>]
     public partial class CreateStoreCustomerOrderModelLocalizer
     {
-
+        public static string OrderSubTotalText => Localizer![OrderSubTotalTextKey];
+        public static string OrderTotalText => Localizer![OrderTotalTextKey];
+        public static string TaxTotalText => Localizer![TaxTotalTextKey];
+        [ResourceKey(defaultValue: "Order SubTotal")]
+        public const string OrderSubTotalTextKey = "OrderSubTotalText";
+        [ResourceKey(defaultValue: "Order Total")]
+        public const string OrderTotalTextKey = "OrderTotalText";
+        [ResourceKey(defaultValue: "Tax Total")]
+        public const string TaxTotalTextKey = "TaxTotalText";
     }
 }
