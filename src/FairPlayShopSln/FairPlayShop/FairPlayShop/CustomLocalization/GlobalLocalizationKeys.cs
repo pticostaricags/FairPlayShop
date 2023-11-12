dec5bdcb009f78bc1357a1b2bc7ea930060af2a3
Eduardo Fonseca <efonsecab@gmail.com>
2023-11-12 08:01:05 -0600

    Adding localized summary

--- src/FairPlayShopSln/FairPlayShop/FairPlayShop/CustomLocalization/GlobalLocalizationKeys.cs
+++ src/FairPlayShopSln/FairPlayShop/FairPlayShop/CustomLocalization/GlobalLocalizationKeys.cs
@@ -8,7 +8,10 @@ namespace FairPlayShop.CustomLocalization
         protected GlobalKeysLocalizer() { }
         public static IStringLocalizer<GlobalKeysLocalizer>? Localizer { get; set; }
         public static string SaveText => Localizer![SaveTextKey];
+        public static string PaginatorSummaryText => Localizer![PaginatorSummaryTextKey];
         [ResourceKey(defaultValue: "Save")]
         public const string SaveTextKey = "SaveText";
+        [ResourceKey("Page {0} of {1}. Total Items: {2}")]
+        public const string PaginatorSummaryTextKey = "PaginatorSummaryText";
     }
 }
