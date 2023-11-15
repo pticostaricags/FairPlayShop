0cd58f7747e0c14aa2c2809bfe383dfe67ac5923
Eduardo Fonseca <efonsecab@gmail.com>
2023-11-15 06:35:51 -0600

    Adding localization to login page

---
+++ src/FairPlayShopSln/FairPlayShop.Models/Account/CreateAccountModel.cs
@@ -0,0 +1,48 @@
+﻿using FairPlayShop.Common.CustomAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayShop.Models.Account
+{
+    public partial class CreateAccountModel
+    {
+        [Required(ErrorMessageResourceType = typeof(CreateAccountModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateAccountModelLocalizer.Email_Required))]
+        [EmailAddress(ErrorMessageResourceType = typeof(CreateAccountModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateAccountModelLocalizer.Email_EmailAddress))]
+        [Display(ResourceType = typeof(CreateAccountModelLocalizer),
+            Name = nameof(CreateAccountModelLocalizer.Email))]
+        public string Email { get; set; } = "";
+
+        [Required(ErrorMessageResourceType = typeof(CreateAccountModelLocalizer),
+            ErrorMessageResourceName = nameof(CreateAccountModelLocalizer.Password_Required))]
+        [DataType(DataType.Password)]
+        [Display(ResourceType = typeof(CreateAccountModelLocalizer),
+            Name = nameof(CreateAccountModelLocalizer.Password))]
+        public string Password { get; set; } = "";
+
+        [Display(ResourceType = typeof(CreateAccountModelLocalizer),
+            Name = nameof(CreateAccountModelLocalizer.RememberMe))]
+        public bool RememberMe { get; set; }
+    }
+
+    [CompilerGenerated]
+    [LocalizerOfT<CreateAccountModel>]
+    public partial class CreateAccountModelLocalizer
+    {
+        public static string RememberMe => Localizer[RememberMeTextKey];
+        public static string Email => Localizer[EmailTextKey];
+        public static string Password => Localizer[PasswordTextKey];
+        [ResourceKey(defaultValue: "Remember me?")]
+        public const string RememberMeTextKey = "RememberMeText";
+        [ResourceKey(defaultValue: "Email")]
+        public const string EmailTextKey = "EmailText";
+        [ResourceKey(defaultValue: "Password")]
+        public const string PasswordTextKey = "PasswordText";
+    }
+}
